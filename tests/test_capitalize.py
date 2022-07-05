b2202062a822fc0cacbb4492b07159e056c4d0f2
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-07-05 09:06:36 +0600

    Use assert instead of "if/else"
    Learned how to write tests using "assert" syntax.

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,7 @@
 from hexlet_auto_test.package.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
 
 print('Все тесты пройдены!')
